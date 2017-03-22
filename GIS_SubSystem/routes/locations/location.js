2cecdfa8c9f42045aa20693ab9a4e15b5412abd6
Rian <u15101283@tuks.co.za>
2017-03-22 22:18:31 +0200

    feat(crud): added update and delete function
    added update function as well as route
    added delete function as well as route

--- GIS_SubSystem/routes/locations/location.js
+++ GIS_SubSystem/routes/locations/location.js
@@ -11,5 +11,11 @@ router.post('/', location.create); // => PUSH to /create creates a new location.
 debug('Adding location route: GET / (Description: where location is retrieved via id)');
 router.get('/:id', location.getById); // => GET to /:id
 
+debug('Deleting location route: DELETE / (Description: where location is deleted)');
+router.delete('/', location.delete); // => DELETE to /delete a location.
+
+debug('Updating location route: PATCH / (Description: where location is updated)');
+router.patch('/', location.patch); // => PATCH to /update a location
+
 debug('Location router exported');
 module.exports = router;
