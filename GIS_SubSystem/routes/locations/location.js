b1d808f77cc0001ffd1ed9f85cc7a8a738dc2d55
Mankgwanyane Tlaka <u14351872@tuks.co.za>
2017-03-23 12:07:29 +0200

    Update location.js

--- GIS_SubSystem/routes/locations/location.js
+++ GIS_SubSystem/routes/locations/location.js
@@ -11,5 +11,8 @@ router.post('/', location.create); // => PUSH to /create creates a new location.
 debug('Adding location route: GET / (Description: where location is retrieved via id)');
 router.get('/:id', location.getById); // => GET to /:id
 
+debug('Adding location route: GET / (Description: where location is retrieved via name)');
+router.get('/:name', location.getByName); // => GET to /:name
+
 debug('Location router exported');
 module.exports = router;
